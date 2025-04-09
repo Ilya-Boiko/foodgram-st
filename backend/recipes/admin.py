cd4246bab946e5ec93c5a4d8364cfd908230befc
Ilya-Boiko <ilabojko62@gmail.com>
2025-04-10 04:25:28 +0700

    2

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -8,6 +8,8 @@ class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
     extra = 1
     min_num = 1
+    verbose_name = 'Ингредиент'
+    verbose_name_plural = 'Ингредиенты'
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
@@ -15,7 +17,8 @@ class RecipeAdmin(admin.ModelAdmin):
     list_filter = ('author',)
     search_fields = ('name', 'author__username')
     inlines = [RecipeIngredientInline]
-    
+    exclude = ('in_shopping_cart', 'favorited')
+    readonly_fields = ('favorite_count',)
     def get_queryset(self, request):
         return super().get_queryset(request).annotate(
             favorite_count=Count('favorited')
@@ -23,4 +26,5 @@ class RecipeAdmin(admin.ModelAdmin):
 
     def favorite_count(self, obj):
         return obj.favorite_count
-    favorite_count.short_description = 'В избранном'
+    favorite_count.short_description = 'Кол-во добавлений в избранное'
+
